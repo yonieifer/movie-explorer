a2671c8dca8d1b3f36cad8c994867020111378a1
Yonatan Eiferman <yonieifer@gmail.com>
2026-09-02 19:52:22 +0300

    add initial Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <>
-        
+        <Link to="/">Movies Explorer</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="favortes">Favorites</Link>
     </>
   )
 }
